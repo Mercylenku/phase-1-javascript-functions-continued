bfeab722f3453895c569986083d68657c7d7294f
Moringa School <mercylenku855@gmail.com>
2022-09-27 13:04:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(doWhat = 'go to the office'){
+    console.log(`This Monday, I will ${doWhat}.`);
+    return `This Monday, I will ${doWhat}.`;
+}
+function wrapAdjective(par1 = "*"){
+    return function (par2 = "a hard worker"){
+    return `You are ${par1}${par2}${par1}!`
+    }
+}
